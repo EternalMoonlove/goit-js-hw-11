23c4e0cd79d540900bc051132892edd99e280bf8
Denis <denisklepach060203@icloud.com>
2025-06-11 14:49:39 +0300

    update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,4 +4,26 @@
 
 import axios from 'axios';
 
-function getImagesByQuery(query) {}
+const PIXABAY_API_URL = 'https://pixabay.com/api/';
+const PIXABAY_API_KEY = '50784112-005c51f7743801558c0ea8079';
+
+export async function getImagesByQuery(query) {
+  const encodedQuery = encodeURIComponent(query);
+
+  try {
+    const response = await axios.get(PIXABAY_API_URL, {
+      params: {
+        key: PIXABAY_API_KEY,
+        q: encodedQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Помилка під час отримання зображень:', error);
+    throw error;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,53 @@
 /*
 У файлі main.js напиши всю логіку роботи додатка. Виклики нотифікацій iziToast, усі перевірки на довжину масиву в отриманій відповіді робимо саме в цьому файлі. Імпортуй в нього функції із файлів pixabay-api.js та render-functions.js та викликай їх у відповідний момент.
 */
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import { getImagesByQuery } from './js/pixabay-api.js';
 
 const formEl = document.querySelector('.js-form');
 const inputEl = document.querySelector('input[name="search-text"]');
+const listEl = document.querySelector('.js-gallery');
+
+formEl.addEventListener('submit', async event => {
+  event.preventDefault();
+
+  const inputValue = inputEl.value.trim();
+
+  if (inputValue === '') {
+    iziToast.error({
+      title: 'Error',
+      message: 'Будь ласка, введіть щось для пошуку!',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  try {
+    const responseData = await getImagesByQuery(inputValue);
+
+    if (responseData.hits.length === 0) {
+      iziToast.info({
+        title: 'No results',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+        backgroundColor: '#ef4040',
+        messageColor: '#fff',
+        titleColor: '#fff',
+      });
+    } else {
+      console.log('Знайдено зображення:', responseData.hits);
+    }
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Failed to fetch images. Please try again later.',
+      position: 'topRight',
+    });
+    console.error('Помилка під час пошуку зображень:', error);
+  } finally {
+    inputEl.value = '';
+  }
+});
